6bd87905164e3bcad264b6c9ce5a7d7abee3c1fa
Mattia Biondi <mattiabiondi1@gmail.com>
2020-11-24 12:10:39 +0100

    Add global constants file

---
+++ src/gui/const.h
@@ -0,0 +1,16 @@
+/*
+ * This header file contains the global constant definitions.
+ */
+
+#define MAXRECENTFILES 5
+
+#define MINCARS 0
+#define MAXCARS 10
+
+#define MINCROSSROADS 0
+#define MAXCROSSROADS 10
+#define DEFAULTSPEEDLIMIT 70
+#define MINSPEEDLIMIT 15
+#define MAXSPEEDLIMIT 130
+#define FRICTIONASPHALT 72
+#define FRICTIONGRASS 35
